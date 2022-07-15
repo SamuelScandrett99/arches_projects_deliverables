1bd93456a0d5dbf98323d96b5b5279d0761b652d
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2022-07-15 10:09:27 +0100

    Removed test function from view

--- jersey/views/persistent_overlay_order/reorder_maps.py
+++ jersey/views/persistent_overlay_order/reorder_maps.py
@@ -13,20 +13,11 @@ from arches.app.models.models import MapLayer
 from arches.app.views.base import BaseManagerView
 
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
-
-
-import datetime
 from django.http import HttpResponse
 
 #Decorators
 @method_decorator(can_edit_resource_instance, name="dispatch")
 class ReorderMaps(BaseManagerView):
-    def current_datetime(request):
-        now = datetime.datetime.now()
-        html = "<html><body>It is now %s.</body></html>" % now
-        print(html)
-        return HttpResponse(html)
-    
     def post(self, request):
         json = request.body
         data = JSONDeserializer().deserialize(json)
@@ -35,6 +26,4 @@ class ReorderMaps(BaseManagerView):
             temp = maps.get(maplayerid = map['maplayerid'])
             temp.sortorder = i
             temp.save()
-            print(vars(temp))
-            # print(map)
         return HttpResponse("OK")
