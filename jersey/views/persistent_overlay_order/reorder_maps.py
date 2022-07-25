130684c0a5896068ace2ee4ec30241873aea03d0
KacperSzyf <45105722+KacperSzyf@users.noreply.github.com>
2022-07-25 10:39:37 +0100

    Added try/catch for Node object

--- jersey/views/persistent_overlay_order/reorder_maps.py
+++ jersey/views/persistent_overlay_order/reorder_maps.py
@@ -9,21 +9,38 @@ from django.utils.translation import ugettext as _
 from arches.app.models.system_settings import settings
 from arches.app.utils.betterJSONSerializer import JSONSerializer
 from arches.app.utils.decorators import can_edit_resource_instance
-from arches.app.models.models import MapLayer
+from arches.app.models.models import MapLayer, Node
 from arches.app.views.base import BaseManagerView
 
 from arches.app.utils.betterJSONSerializer import JSONSerializer, JSONDeserializer
+
+
+import datetime
 from django.http import HttpResponse
 
 #Decorators
 @method_decorator(can_edit_resource_instance, name="dispatch")
 class ReorderMaps(BaseManagerView):
     def post(self, request):
+
         json = request.body
         data = JSONDeserializer().deserialize(json)
         maps = MapLayer.objects.all()
+
         for i, map in enumerate(data['map_order']):
-            temp = maps.get(maplayerid = map['maplayerid'])
-            temp.sortorder = i
-            temp.save()
+            try:
+                temp = maps.get(maplayerid = map['maplayerid'])
+                temp.layersortorder = i
+                temp.save()
+            except MapLayer.DoesNotExist:
+                temp = None
+                
+            try:
+                breakpoint()
+                temp = Node.objects.get(nodeid = map['maplayerid'])
+                temp.layersortorder = i
+                temp.save()
+            except Node.DoesNotExist:
+                temp = None
+                
         return HttpResponse("OK")
